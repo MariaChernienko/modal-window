09e536763f7a7a117066f43b03f97bf37b23050a
Maria <murikmarusia@gmail.com>
2018-11-16 17:01:31 +0200

    Added open/close function

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,30 @@
-console.log("Hello, modal window");
+(function() {
+  const open = document.querySelector('[data-target="openModal"]');
+  const modalWindow = document.querySelector('.modal-container');
+  const close = document.querySelectorAll('[data-target="closeModal"]');
+
+  open.addEventListener('click', (event) => {
+    openModalWindow();
+  });
+  
+    close.forEach(element => {
+      // closeModalWindow();
+      element.addEventListener('click', (event) => {
+        modalWindow.classList.remove('active');
+      });
+      
+    });
+
+
+
+  const openModalWindow = function() {
+    modalWindow.classList.add('active');
+  };
+  // const closeModalWindow = function() {
+  //   element.addEventListener('click', (event) => {
+  //     modalWindow.classList.remove('active');
+  //   });
+  // };
+
+
+}());
