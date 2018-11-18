af73b8a4df9be9bfbc2be2994d0df297328005c9
Maria <murikmarusia@gmail.com>
2018-11-19 00:08:32 +0200

    Added second modal

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,30 +1,22 @@
-(function() {
-  const open = document.querySelector('[data-target="openModal"]');
-  const modalWindow = document.querySelector('.modal-container');
-  const close = document.querySelectorAll('[data-target="closeModal"]');
+(function () {
+  const modalFirstWindow = document.querySelector('[data-target="firstWindow"]');
+  const modalSecondWindow = document.querySelector('[data-target="secondWindow"]');
+  const allBtns = document.querySelectorAll('[data-target]');
 
-  open.addEventListener('click', (event) => {
-    openModalWindow();
-  });
-  
-    close.forEach(element => {
-      // closeModalWindow();
-      element.addEventListener('click', (event) => {
-        modalWindow.classList.remove('active');
-      });
-      
+
+  allBtns.forEach((element) => {
+    element.addEventListener('click', () => {
+      const targetName = element.dataset.target;
+      if (targetName == 'openFirstModal') modalFirstWindow.classList.add('active');
+      else if (targetName == 'closeFirstModal') modalFirstWindow.classList.remove('active');
+      else if (targetName == 'openSecondModal') modalSecondWindow.classList.add('active');
+      else if (targetName == 'closeSecondModal') modalSecondWindow.classList.remove('active');
+      else if (targetName == 'showAlert') {
+        modalFirstWindow.classList.remove('active');
+        setTimeout(() => {
+          alert('Hello');
+        }, 100);
+      }
     });
-
-
-
-  const openModalWindow = function() {
-    modalWindow.classList.add('active');
-  };
-  // const closeModalWindow = function() {
-  //   element.addEventListener('click', (event) => {
-  //     modalWindow.classList.remove('active');
-  //   });
-  // };
-
-
+  });
 }());
